054a741fc7ba6219bdf9ab59925a99206a0c37e9
吉市 <rescue.masahiro.441@gmail.com>
2022-01-27 00:36:26 +0000

    デプロイ後調整1

--- resources/views/home/top.blade.php
+++ resources/views/home/top.blade.php
@@ -6,8 +6,8 @@
  <div class="jumbotron jumbotron-extend">
     <div class="container-fluid jumbotron-container">
         <div class="row">
-            <div class="col-md-6">
-                <h2 class="top-title">
+            <div class="col-md-6 col-6">
+                <h2>
                     <span>最高のワークアウトを全ての人へ</span>
                 </h2>
                 <a type="button" class="btn btn-light btn-lg" href="/introduction">Let's Workout</a>
